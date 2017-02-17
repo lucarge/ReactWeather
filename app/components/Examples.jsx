32018834ab5ea66be0424faefacb04bfb6c3eed4
Luca Argenziano <lucaargenziano@MBPdiLarodiLuca.station>
2017-02-17 19:45:47 +0100

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h2>Examples Component</h2>
+    <div>
+      <h2>Examples</h2>
+      <p>This will be the examples page.</p>
+    </div>
   );
 };
 
